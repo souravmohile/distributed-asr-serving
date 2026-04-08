3d7943ca2ee6eaafdd7ef735dce1d8abdef69edf
Sourav Mohile <souravmohile@Souravs-MacBook-Pro.local>
2026-04-08 22:33:18 +0530

    openai-base inference test

--- src/test.py
+++ src/test.py
@@ -1,16 +1,18 @@
+import torch
 from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
 import librosa
+from dotenv import load_dotenv
 
-processor = AutoProcessor.from_pretrained("openai/whisper-tiny")
-model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-tiny")
+load_dotenv()
+
+processor = AutoProcessor.from_pretrained("openai/whisper-base", cache_dir="./models/")
+model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-base", cache_dir="./models/")
 
 audio, sampling_rate = librosa.load("data/audio_files/audio_file_1.mp3", sr=16000)
 
 input_features = processor(audio, sampling_rate=sampling_rate, return_tensors="pt").input_features
 
-# 4. Generate token IDs
 predicted_ids = model.generate(input_features)
 
-# 5. Decode token IDs to text
 transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
 print(transcription[0])
